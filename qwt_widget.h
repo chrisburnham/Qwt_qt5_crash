21edcb5c166435e3c20bde53497e6401bbddafdd
Chris Burnham <cburnham@greensea.com>
2019-10-21 16:54:41 -0400

    only h file

--- qwt_widget.h
+++ qwt_widget.h
@@ -3,16 +3,20 @@
 
 #include <qwt_plot.h>
 
+#include <qwt_plot_zoomer.h>
+#include <qwt_plot_canvas.h>
+
 class Qwt_widget : public QwtPlot
 {
   Q_OBJECT
 
 public:
   /**Constructor*/
-  Qwt_widget(QWidget * = 0);
-
-  /**Destructor*/
-  ~Qwt_widget();
+  Qwt_widget(QWidget* parent = 0) : QwtPlot(parent)
+  {
+    QwtPlotCanvas canvas;
+    QwtPlotZoomer* zoomer = new QwtPlotZoomer(&canvas);
+  }
 };
 
 #endif // QWT_WIDGET_H
